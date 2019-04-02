688198822ece01a90986b338119d4d8c421a7547
evox1994 <dmitrybatukhtin@gmail.com>
2019-04-02 16:35:07 +0700

    sdfvsfdv

--- js/main.js
+++ js/main.js
@@ -1,2 +1,64 @@
 $(document).ready(function(){
+
+	$('.radio-btn').click(function(){
+		if ( $(this).hasClass('active') ) {
+			$(this).removeClass('active');
+		} else {
+			$(this).removeClass('error');
+			$(this).addClass('active');
+		}
+	});
+
+	$('.mobile-btn').click(function(){
+		if ( $(this).hasClass('active') ){
+			$(this).removeClass('active');
+			$('.mobile-menu').removeClass('active');
+			$('body').removeClass('no-scroll');
+		} else {
+			$(this).addClass('active');
+			$('.mobile-menu').addClass('active');
+			$('body').addClass('no-scroll');
+		}
+	});
+
+	$('.fancybox').fancybox();
+	$('input[type="tel"]').inputmask('+7 (999) 999-99-99');
+
+	$('.close-btn').click(function(){
+		$('.mobile-btn').removeClass('active');
+		$('.mobile-menu').removeClass('active');
+		$('body').removeClass('no-scroll');
+	});
+
+	$('input').on('input',function(){
+		$(this).removeClass('error');
+	});
+	$('textarea').on('input',function(){
+		$(this).removeClass('error');
+	});
+
+	$('form button[type="submit"]').click(function(){
+		if ( $(this).parents('form').find('.button-text .radio-btn').hasClass('active') ) {
+			$(this).parents('form').find('input').each(function(){
+				if(!$(this).val().length) { 
+					event.preventDefault(); 
+					$(this).addClass("error"); 
+				} else { 
+					$(this).removeClass("error"); 
+				} 
+			});
+			$(this).parents('form').find('textarea').each(function(){
+				if(!$(this).val().length) { 
+					event.preventDefault(); 
+					$(this).addClass("error"); 
+				} else { 
+					$(this).removeClass("error"); 
+				} 
+			});
+		} else {
+			$(this).parents('form').find('.button-text .radio-btn').addClass('error');
+			event.preventDefault();
+		}
+	});
+
 });
